6ee9d71390dd2621f41accf83bace91f97c90292
Othavio Quiliao <othavioaqa@gmail.com>
2024-10-24 10:10:11 -0300

    FInal ajustes

--- src/lib/components/TabPlanos/CPF1/MelhorPreco.svelte
+++ src/lib/components/TabPlanos/CPF1/MelhorPreco.svelte
@@ -4,9 +4,11 @@
 <div class="absolute left-0 top-0 h-96 w-full">
 	<div class="relative flex h-96 w-full overflow-hidden">
 		<div
-			class="rotate absolute -right-28 bottom-12 z-10 flex w-full -rotate-45 bg-orange-500 text-center md:bottom-16"
+			class="rotate absolute -right-32 md:-right-36 bottom-12 z-10 flex w-96 -rotate-45 bg-orange-500 text-center md:bottom-10"
 		>
-			<h4 class="w-full font-inter text-sm font-bold uppercase text-background md:text-base">
+			<h4
+				class="w-full font-inter text-sm font-bold uppercase text-background md:text-sm lg:text-base"
+			>
 				Mais Vendido
 			</h4>
 		</div>
